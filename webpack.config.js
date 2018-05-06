ceb7c720e0965995508b95e1b1f20dbec16c4de4
Bnmkt <b.kt@hotmail.com>
2018-05-06 12:34:53 +0200

    Better image optimisation

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,16 @@ const glob = require("glob");
 const ExctractTextPlugin = require("extract-text-webpack-plugin");
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const imageminJpegRecompress = require('imagemin-jpeg-recompress');
 
 const config = {
+    target: "web",
     entry: {
-        main: "./src/index.es6"
+        bundle: "./src/index.es6"
     },
     output: {
         path: path.resolve(__dirname, "./public/assets/js"),
-        filename: "./bundle.js"
+        filename: "./[name].js"
     },
     plugins: [
         new UglifyJSPlugin(),
@@ -25,7 +27,16 @@ const config = {
                 sources: glob.sync('src/images/**/*.*'),
                 destination: 'public/assets'
             },
-            jpegtran: {progressive: true}
+            plugins:[
+                imageminJpegRecompress({
+                    min:50,
+                    max:80
+                })
+            ],
+            jpegtran: {progressive: true},
+            pngquant: {
+                quality: '70-90'
+            }
         })
     ],
     module: {
